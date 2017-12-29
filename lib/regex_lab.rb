55a284996412dfa5557e7ae10afff1dca79a6b2f
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-29 15:46:54 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if word.match(/^un$ing/) then true else false end
+  if text.match(/^un$ing/) then true else false end
 end
 
 def words_five_letters_long(text)
